d2c4e15ae926320326c234f4e2a4d818f9a3829e
AaronF1 <64245778+AaronF1@users.noreply.github.com>
2023-02-25 18:44:44 +0100

    Update Streamlit_Front_End.py

--- Streamlit_Front_End.py
+++ Streamlit_Front_End.py
@@ -22,9 +22,11 @@ with col1:
 with col2:
     st.image(img_path2, use_column_width=True)
 
-
 # Create the main title for the dashboard
 st.title("Radio Link Investment Decision Tool")
+
+data["Total Number of Concessions Owned"] = data['Number of concession']
+
 
 # Create the options on the left side of the dashboard
 st.sidebar.title("Filter Options")
@@ -34,10 +36,10 @@ frequencies = sorted(data["Frequency GHZ rounded"].unique())
 default_frequencies = [15, 22, 23, 24]
 selected_frequencies = st.sidebar.multiselect("Select Frequency GHZ Rounded:", frequencies, default=default_frequencies)
 concession_min, concession_max = st.sidebar.slider(
-    "Select Number of Concessions Range:",
-    min(data["Number of concession"]),
-    max(data["Number of concession"]),
-    (min(data["Number of concession"]), max(data["Number of concession"])),
+    "Select Total Number of Concessions Owned Range:",
+    min(data["Total Number of Concessions Owned"]),
+    max(data["Total Number of Concessions Owned"]),
+    (min(data["Total Number of Concessions Owned"]), max(data["Total Number of Concessions Owned"])),
     step=1,
 )
 population = st.sidebar.multiselect("Select Population:", ["Select All"] + sorted(data["Population"].unique().tolist()), default=["Select All"])
@@ -54,7 +56,7 @@ if "Select All" not in owner:
 if "Select All" not in municipality:
     filtered_data = filtered_data[filtered_data["Municipality"].isin(municipality)]
 filtered_data = filtered_data[filtered_data['Frequency GHZ rounded'].isin(selected_frequencies)]
-filtered_data = filtered_data[(filtered_data['Number of concession'] >= concession_min) & (filtered_data['Number of concession'] <= concession_max)]
+filtered_data = filtered_data[(filtered_data['Total Number of Concessions Owned'] >= concession_min) & (filtered_data['Total Number of Concessions Owned'] <= concession_max)]
 if "Select All" not in population:
     filtered_data = filtered_data[filtered_data["Population"].isin(population)]
 
@@ -68,10 +70,10 @@ filtered_data['time_until_termination'] = filtered_data['time_until_termination'
 filtered_data['number_of_concessions_opened_last_2_years'] = filtered_data.groupby('Owner')['days_since_last_opening'].transform(lambda x: (x < 730).sum())
 filtered_data['average_time_until_termination'] = filtered_data.groupby('Owner')['time_until_termination'].transform('mean')
 filtered_data['average_days_since_last_opening'] = filtered_data.groupby('Owner')['days_since_last_opening'].transform('mean')
-filtered_data['number_of_concessions_within_frequency_range'] = filtered_data.groupby('Owner')['Frequency GHZ rounded'].transform(lambda x: x.isin(selected_frequencies).sum())
+filtered_data['Total Number of Concessions Owned within Frequency Range'] = filtered_data.groupby('Owner')['Frequency GHZ rounded'].transform(lambda x: x.isin(selected_frequencies).sum())
 
 
-filtered_data['percentage_of_concessions_within_frequency_range'] = (filtered_data['number_of_concessions_within_frequency_range'] / filtered_data['Number of concession'] * 100).round(2)
+filtered_data['Percentage of Concessions within Frequency Range relative To Total Owned'] = (filtered_data['Total Number of Concessions Owned within Frequency Range'] / filtered_data['Total Number of Concessions Owned'] * 100).round(2)
 
 filtered_data['small_village'] = filtered_data.groupby('Owner')['Population'].transform(lambda x: (x == 'small village').sum())
 filtered_data['village'] = filtered_data.groupby('Owner')['Population'].transform(lambda x: (x == 'village').sum())
@@ -86,10 +88,10 @@ df_ranking_normalised = pd.DataFrame()
 df_ranking_normalised['Owner'] = filtered_data2.index
 df_ranking_normalised['rank_average_time_until_termination'] = (filtered_data2['average_time_until_termination'].values-filtered_data2['average_time_until_termination'].min())/(filtered_data2['average_time_until_termination'].max()-filtered_data2['average_time_until_termination'].min())
 df_ranking_normalised['rank_average_days_since_last_opening'] = (filtered_data2['average_days_since_last_opening'].values-filtered_data2['average_days_since_last_opening'].min())/(filtered_data2['average_days_since_last_opening'].max()-filtered_data2['average_days_since_last_opening'].min())
-if (filtered_data2['percentage_of_concessions_within_frequency_range'].max()-filtered_data2['percentage_of_concessions_within_frequency_range'].min()) == 0:
+if (filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].max()-filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].min()) == 0:
     df_ranking_normalised['rank_percentage_of_concessions_within_frequency_range'] = 1
 else:
-    df_ranking_normalised['rank_percentage_of_concessions_within_frequency_range'] = (filtered_data2['percentage_of_concessions_within_frequency_range'].values-filtered_data2['percentage_of_concessions_within_frequency_range'].min())/(filtered_data2['percentage_of_concessions_within_frequency_range'].max()-filtered_data2['percentage_of_concessions_within_frequency_range'].min())
+    df_ranking_normalised['rank_percentage_of_concessions_within_frequency_range'] = (filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].values-filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].min())/(filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].max()-filtered_data2['Percentage of Concessions within Frequency Range relative To Total Owned'].min())
 filtered_data = filtered_data.merge(df_ranking_normalised, on="Owner", how='left')
 
 # Define the investment score calculation
@@ -105,7 +107,7 @@ filtered_data['investment_score'] = filtered_data.apply(lambda row: investment_s
 filtered_data.sort_values(by='investment_score', ascending=False, inplace=True)
 
 # Create the table with the filtered data
-st.dataframe(filtered_data[['Reference', 'Owner', 'Municipality', 'Frequency GHZ rounded', 'Number of concession', 'number_of_concessions_within_frequency_range', 'percentage_of_concessions_within_frequency_range', 'investment_score', 'rank_average_time_until_termination', 'rank_average_days_since_last_opening', 'rank_percentage_of_concessions_within_frequency_range']])
+st.dataframe(filtered_data[['Reference', 'Owner', 'Municipality', 'Frequency GHZ rounded', 'Total Number of Concessions Owned', 'Total Number of Concessions Owned within Frequency Range', 'Percentage of Concessions within Frequency Range relative To Total Owned', 'investment_score', 'rank_average_time_until_termination', 'rank_average_days_since_last_opening', 'rank_percentage_of_concessions_within_frequency_range']])
 
 def download_excel(data):
     output = pd.DataFrame(data)
